163572de98668737b0617e6877cec7c6b365fb6c
chrisPfaff <chrisPfaff10@gmail.com>
2022-03-29 10:20:07 -0500

    added browserslist parcel react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+
+
