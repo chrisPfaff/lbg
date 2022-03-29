985073758ea80a5e47eec926aa22f215cbad3b0d
chrisPfaff <chrisPfaff10@gmail.com>
2022-03-29 15:22:58 -0500

    error boundary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-
+import About from "./components/About";
 
 const App = () => {
   return (
-    <Header/>
-  )
-}
+    <StrictMode>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/about/" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
 
 render(<App />, document.getElementById("root"));
-
-
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  
-  return  (
+  return (
     <div className="header">
-      <h1>Header</h1>
+      <Link to={`/`}>Home</Link>
+      <Link to={`/about/`}>About</Link>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
