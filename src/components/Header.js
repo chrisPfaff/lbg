438c6bd1a6967ca794df2265cf9be3a55524aca0
Chris Pfaff <chrispfaff10@gmail.com>
2022-06-14 19:35:34 -0500

    making sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom";
 import "./styles/header.scss";
 import flower from "../assets/flower.svg";
+import { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  });
+
+  const isSticky = (e) => {
+    const header = document.querySelector(".header");
+    const scrollTop = window.scrollY;
+    scrollTop >= 140
+      ? header.classList.add("is-sticky")
+      : header.classList.remove("is-sticky");
+  };
+
   return (
     <div className="header">
       <div className="container">
